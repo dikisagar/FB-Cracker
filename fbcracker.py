05e29a7e059bfa9e5cac5ee42f7a145cd6fdcf48
dikisagar <72176900+dikisagar@users.noreply.github.com>
2020-10-03 09:06:27 +0530

    Add a parenthesis in print section..

--- fbcracker.py
+++ fbcracker.py
@@ -22,7 +22,7 @@ br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U
 info = time.strftime("%S:%M:%H")
 
 def quit():
-    print '\x1b[1;91m[!] Program Closed'
+    print ('\x1b[1;91m[!] Program Closed')
     os.sys.exit()
 
 
